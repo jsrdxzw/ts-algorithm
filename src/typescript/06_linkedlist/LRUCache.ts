65bec628cbffa654f79fb28894f80802a52cc524
xuzhiwei <jsrdxzw@163.com>
2019-05-15 23:19:11 +0900

    add lruCache

--- src/typescript/06_linkedlist/LRUCache.ts
+++ src/typescript/06_linkedlist/LRUCache.ts
@@ -4,7 +4,7 @@
  */
 class LRUCache<K, V> {
   private cacheMap: Map<K, LinkedListNode<K, V>>
-  private limit: number
+  private readonly limit: number
   private head: LinkedListNode<K, V> | null = null
   private end: LinkedListNode<K, V> | null = null
 
@@ -18,7 +18,26 @@ class LRUCache<K, V> {
     const node = this.cacheMap.get(key)
     if (!node) return null
     this.refreshNode(node)
-    return null
+    return node.value
+  }
+
+  public put(key: K, value: V) {
+    const node = this.cacheMap.get(key)
+    // 原缓存不存在则加入到队尾
+    if (!node) {
+      // 大于规定的size则删除最不常用的
+      if (this.cacheMap.size >= this.limit) {
+        const oldKey = this.removeNode(this.head!)
+        this.cacheMap.delete(oldKey)
+      }
+      // 在队尾添加
+      const newNode = new LinkedListNode(key, value)
+      this.addNode(newNode)
+      this.cacheMap.set(key, newNode)
+    } else {
+      node.value = value
+      this.refreshNode(node)
+    }
   }
 
   private refreshNode(node: LinkedListNode<K, V>) {
@@ -27,19 +46,34 @@ class LRUCache<K, V> {
     this.addNode(node)
   }
 
-  private removeNode(node: LinkedListNode<K, V>) {
+  private removeNode(node: LinkedListNode<K, V>): K {
     if (node === this.end) {
-      this.end = node
+      this.end = this.end.prev
     } else if (node === this.head) {
       this.head = this.head.next
     } else {
-      if (node.prev) node.prev.next = node.next
-      if (node.next) node.next.prev = node.prev
+      // 这个由于排除了首尾节点
+      node.prev!.next = node.next
+      node.next!.prev = node.prev
     }
+    return node.key
   }
 
+  /**
+   * 这里向尾部追加节点
+   * @param node
+   */
   private addNode(node: LinkedListNode<K, V>) {
-    // TODO
+    if (this.end) {
+      this.end.next = node
+      node.prev = this.end
+    }
+    this.end = node
+    if (this.head === null) {
+      this.head = node
+    }
+    // 消除之前的节点的下一个引用对象,防止无限循环
+    node.next = null
   }
 }
 
@@ -61,3 +95,12 @@ class LinkedListNode<K, V> {
     this.prev = prev
   }
 }
+
+const cache = new LRUCache<string,string>(3)
+cache.put('lv','xzw')
+cache.put('lv2','xzw2')
+cache.put('lv3','xzw3')
+cache.put('lv4','xzw4')
+cache.put('lv5','xzw5')
+
+console.log(cache)
