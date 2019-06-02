9cbeae53787a6b4854c71def7a7fd57fe85314ca
xuzhiwei <jsrdxzw@163.com>
2019-06-02 12:19:13 +0900

    add counting sort

---
+++ src/typescript/13_sorts/BucketSort.ts
@@ -0,0 +1,13 @@
+/**
+ * 桶排序对数据的要求比较高
+ * 首先要知道数据的范围
+ * 然后根据范围将数据分到小范围的桶中
+ * 每个桶采用快速排序
+ * 当桶的数量接近数据量的时候，时间复杂度为O(n)
+ */
+class BucketSort {
+  sort(array: number[], bucketNum = 10) {
+    const length = array.length
+    
+  }
+}
