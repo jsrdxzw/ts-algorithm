d55f9940e6f9b0bba64fc9f7295a7ba847a73454
許　志偉 <01027698@CA4857.local>
2019-06-11 18:30:46 +0900

    want to add binary search that solves more problem

---
+++ src/typescript/15_binarysearch/BinaryFind.ts
@@ -0,0 +1,13 @@
+/**
+ * 二分查找法在实际的场合使用不多
+ * 我们更多的是看到一些查找法的变种
+ */
+class BinaryFind {
+  /**
+   * 在数组中查找第一个等于给定值的位置
+   * @param array 给定的要查找的范围数组
+   * @param target 要查找的目标值
+   * @return 目标在数组中的位置
+   */
+  static findFirstElement(array: number[], target: number): number {}
+}
